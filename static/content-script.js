20c079f7a925121257804f40c050e5cd346b4239
ZCY01 <292695903@qq.com>
2020-08-14 15:27:20 +0800

    调整代码结构

--- static/content-script.js
+++ static/content-script.js
@@ -2,87 +2,235 @@ import { suspend, mutationsPromise, simulateClick, parseActivityId, storage } fr
 import { USER_STATUS, ACTIVITY_STATUS } from './config'
 import { Toast } from 'vant';
 
-function checkLoginStatus() {
+const task_opts = {
+	async auto_login() {
 
-	if (document.querySelector("#ttbar-login") === null) { // 检查不到用户登录状态
-		return undefined
-	}
+		simulateClick(document.querySelector('.login-tab.login-tab-r a'), true)
 
-	let loginStatus = {
-		status: USER_STATUS.LOGOUT,
-		description: '未检查到用户名，点击打开网页登录',
-		shortDescription: `未登录`,
-		timestamp: Date.now()
-	}
+		let res = await storage.get({ autoLogin: false })
+		if (!res.autoLogin) {
+			return
+		}
 
-	if (document.querySelector(".nickname")
-		&& document.querySelector(".nickname").innerText) {
+		await suspend(2000)
 
-		loginStatus.status = USER_STATUS.LOGIN
-		loginStatus.description = `${document.querySelector('.nickname').innerText}`
-		loginStatus.shortDescription = '已登录'
+		res = await storage.get({ account: { username: '', password: '' } })
+		if (!res.account.username || !res.account.password) {
+			return
+		}
+		document.querySelector("#loginname").value = res.account.username
+		document.querySelector("#nloginpwd").value = res.account.password
 
-	}
+		simulateClick(document.querySelector(".login-btn a"), true)
 
-	const login = loginStatus.status === USER_STATUS.LOGIN
-	chrome.runtime.sendMessage({
-		action: "bg_login_status_retrieval",
-		href: window.location.href,
-		loginStatus: loginStatus,
-		status: login
-	})
-	console.log(`登录状态 ${login}`)
-	return login
-}
+		setTimeout(() => {
+			console.warn('自动登录失败')
+		}, 3000)
 
-function newActivityListRetrieval() {
-	console.log(`即将执行 newActivityListRetrieval`)
+		return true
+	},
 
-	let activityList = []
-	document.querySelectorAll('.try-item').forEach(activity => {
-		let link = activity.querySelector('.link')
-		if (activity.innerHTML.indexOf('已申请') < 0) {
-			activityList.push({
-				url: link.href,
-				id: parseInt(activity.parentElement.getAttribute('activity_id')),
+	async check_login_status() {
+		if (document.querySelector("#ttbar-login") === null) { // 检查不到用户登录状态
+			return undefined
+		}
+		let loginStatus = {
+			status: USER_STATUS.LOGOUT,
+			description: '未检查到用户名，点击打开网页登录',
+			shortDescription: `未登录`,
+			timestamp: Date.now()
+		}
+		if (document.querySelector(".nickname")
+			&& document.querySelector(".nickname").innerText) {
+
+			loginStatus.status = USER_STATUS.LOGIN
+			loginStatus.description = `${document.querySelector('.nickname').innerText}`
+			loginStatus.shortDescription = '已登录'
+		}
+		else if(document.querySelector('.link-login')
+			&& document.querySelector('.link-login') === "你好，请登录"){
+
+			let res = await storage.get({ autoLogin: false })
+			if(res.autoLogin){
+				simulateClick(document.querySelector('.link-login'))
+			}
+
+		}
+
+		const login = loginStatus.status === USER_STATUS.LOGIN
+		chrome.runtime.sendMessage({
+			action: "bg_login_status_retrieval",
+			href: window.location.href,
+			loginStatus: loginStatus,
+			status: login
+		})
+		console.log(`登录状态 ${login}`)
+		return login
+	},
+
+	follow_vender_num_retrieval() {
+		let num = 0
+		document.querySelectorAll('#categoryFilter li').forEach(li => {
+			const reg = /（(\d*)）/.exec(li.innerText)
+			if (reg) {
+				num += parseInt(reg[1])
+			}
+		})
+		console.log(`当前用户一共关注了 ${num} 个店铺`)
+		chrome.runtime.sendMessage({
+			action: "bg_follow_vender_num_retrieval",
+			followVenderNum: num
+		})
+		return true
+	},
+	async empty_follow_vender_list() {
+		let res = await emptyFollowVenderList()
+		if (!res) {//要么成功自动reload, 要么失败手动reload
+			window.location.reload()
+		}
+		return res
+	},
+	activity_retrieval() {
+		let activityList = []
+		document.querySelectorAll('.try-item').forEach(activity => {
+			let link = activity.querySelector('.link')
+			if (activity.innerHTML.indexOf('已申请') < 0) {
+				activityList.push({
+					url: link.href,
+					id: parseInt(activity.parentElement.getAttribute('activity_id')),
+					pic_url: activity.querySelector('img').src,
+					name: activity.querySelector('.p-name').innerText,
+					detail: activity.querySelector('.p-detail').innerText,
+					price: parseFloat(activity.querySelector('.p-price span').innerText.substr(1)),
+					timestamp: parseInt(activity.parentElement.getAttribute('end_time')),
+					status: ACTIVITY_STATUS.APPLY
+				})
+			}
+		})
+		console.log(`页面一共有 ${activityList.length} 个未进行的任务`)
+		const pages = document.querySelector('.page.clearfix .p-wrap .p-skip b')
+		chrome.runtime.sendMessage({
+			action: "bg_new_activity_retrieval",
+			activityList: activityList,
+			href: window.location.href,
+			pageNum:pages ? pages.innerText : 1,
+		})
+		return true
+	},
+	success_activity_retrieval() {
+		let activityList = document.querySelectorAll('.list-detail-item')
+		if (!activityList) {
+			console.warn('无法获得/没有申请成功的商品')
+			return false
+		}
+		let successActivityList = []
+		for (let activity of activityList) {
+			const countDown = activity.querySelector('.re-time')
+			if (!countDown) {//过期了
+				continue
+			}
+			const timestamp = parseInt(countDown.getAttribute('end_time')) + countDown.getAttribute('after_hours') * 60 * 60 * 1000
+			successActivityList.push({
+				url: activity.querySelector('.p-name a').href,
+				id: parseInt(activity.getAttribute('activity_id')),
 				pic_url: activity.querySelector('img').src,
 				name: activity.querySelector('.p-name').innerText,
 				detail: activity.querySelector('.p-detail').innerText,
 				price: parseFloat(activity.querySelector('.p-price span').innerText.substr(1)),
-				timestamp: parseInt(activity.parentElement.getAttribute('end_time')),
-				status: ACTIVITY_STATUS.APPLY
+				timestamp: timestamp,
+				apply: true
 			})
 		}
-	})
-	console.log(`页面一共有 ${activityList.length} 个未进行的任务`)
-	chrome.runtime.sendMessage({
-		action: "bg_new_activity_retrieval",
-		activityList: activityList,
-		href: window.location.href,
-	})
-	const pages = document.querySelector('.page.clearfix .p-wrap .p-skip b')
-	chrome.runtime.sendMessage({
-		action: "bg_page_num_retrieval",
-		pageNum: pages ? pages.innerText : 0,
-		href: window.location.href,
-	})
+		const pages = document.querySelector('.page.clearfix .p-wrap .p-skip b')
+		chrome.runtime.sendMessage({
+			action: "success_activity_retrieval",
+			successActivityList: successActivityList,
+			href: window.location.href,
+			pageNum: pages ? pages.innerText : 1,
+		})
+
+		return true
+	},
+	async activity_apply() {
+		const activityId = parseActivityId(window.location.href)
+
+		let destoryIframeMsg = (status = false, success = false) => {
+			chrome.runtime.sendMessage({
+				action: "bg_activity_applied",
+				href: window.location.href,
+				activityId: activityId,
+				status: status,
+				success: success
+			})
+		}
+
+		let state = document.querySelector('.state')
+		if (!state) {                                 // 获取不了状态，关闭
+			console.log(`${window.location.href} 无法获取状态`)
+			return destoryIframeMsg(false) // window.location.reload()
+		}
+		if (state.innerText.startsWith('您已提交申请')) { // 已申请，关闭并更新状态
+			console.log(`${window.location.href} ${state.innerText}`)
+			return destoryIframeMsg(true)
+		}
+		if (!state.innerText.startsWith('活动已开始')) { // 当前页面不是可以申请的页面，关闭
+			console.log(`${window.location.href} ${state.innerText}`)
+			return destoryIframeMsg(false)
+		}
+
+		let res = await clickApplyBtn(activityId)
+		destoryIframeMsg(res, res)
+		return res
+	}
 }
 
-async function waitForDialog(timeout = 2500) {
-	await mutationsPromise(
-		document.body,
-		{ childList: true },
-		mutation => {
-			for (let node of mutation.addedNodes) {
-				if (node.className.startsWith('ui-dialog')) {
-					return true
+function insert_auto_login_btn() {
+	simulateClick(document.querySelector('.login-tab.login-tab-r a'), true)
+	const autoLoginBtn = document.createElement('p')
+	autoLoginBtn.innerHTML = "<span class='jdtry-login'>让京试记住密码并自动登录</span>"
+	document.querySelector('.item.item-fore5').append(autoLoginBtn)
+	autoLoginBtn.onclick = () => {
+		const username = document.querySelector("#loginname").value
+		const password = document.querySelector("#nloginpwd").value
+		if (!username || !password) {
+			Toast('请填写账号和密码后重试')
+			return
+		}
+		storage.set({ account: { username: username, password: password } }).then(() => {
+			Toast('保存账号和密码成功')
+
+			chrome.runtime.sendMessage({
+				action: "bg_get_login_status",
+			}, loginStatus => {
+				console.log(loginStatus)
+				if (loginStatus.status !== USER_STATUS.LOGIN) {
+					simulateClick(document.querySelector(".login-btn a"), true)
 				}
-			}
-		},
-		timeout,
-		"获得 ui-dialog 弹窗失败")
-}
+			})
+		})
 
+	}
+}
+async function waitForDialog(timeout = 2500) {
+	try{
+		await mutationsPromise(
+			document.body,
+			{ childList: true },
+			mutation => {
+				for (let node of mutation.addedNodes) {
+					if (node.className.startsWith('ui-dialog')) {
+						return true
+					}
+				}
+			},
+			timeout,
+			"获得 ui-dialog 弹窗失败")
+	}
+	catch (e) {
+		return false
+	}
+	return true
+}
 
 async function dealWithDialogAlert(innerText) {
 	if (innerText.indexOf('成功') >= 0) {
@@ -93,18 +241,21 @@ async function dealWithDialogAlert(innerText) {
 			action: "bg_update_saveinfo",
 			data: { fulfilled: true }
 		})
-		throw Error('达到每日上限')
+		console.log('达到每日上限')
+		return false
 	}
 	else if (innerText.indexOf('关注数超过上限了哦') >= 0) {
 		chrome.runtime.sendMessage({           //先将就用这个！
 			action: "bg_update_saveinfo",
 			data: { noMoreVender: true }
 		})
-		throw Error('关注数超过上限了哦～先清理下关注列表吧')
+		console.log('关注数超过上限了哦～先清理下关注列表吧')
+		return false
 	}
-	throw Error(`未知DialogAlert弹窗 ${innerText}`)
+	console.warn(`未知DialogAlert弹窗 ${innerText}`)
+	return false
 }
-//未处理可能存在的 timeout!
+
 async function clickApplyBtn(activityId) {
 
 	// 点击试用
@@ -112,7 +263,8 @@ async function clickApplyBtn(activityId) {
 	if (!applyBtn
 		|| applyBtn.innerText !== '申请试用'
 		|| applyBtn.className.indexOf('app-btn-disabled') >= 0) {
-		throw Error(`当前页面不可操作`)
+		console.log(`当前页面不可操作`)
+		return false
 	}
 	simulateClick(applyBtn, true)
 
@@ -128,7 +280,9 @@ async function clickApplyBtn(activityId) {
 	if (needFollow) {
 
 		console.log('需要关注店铺，即将进行关注店铺操作')
-		await waitForDialog()
+		if(await waitForDialog() === false){
+			return false
+		}
 
 		const dialog = document.querySelector('.ui-dialog')
 		if (dialog.className.indexOf('tipsAlert') >= 0) {  //此时不应该出现 tipsAlert 的！
@@ -137,96 +291,22 @@ async function clickApplyBtn(activityId) {
 
 		const btn = dialog.querySelector('.y')
 		if (!btn || btn.innerText !== '关注并申请' || btn.className.indexOf('app-btn-disabled') >= 0) {
-			throw Error(`未知dialog弹窗：${dialog.innerText}`)
+			console.log(`未知dialog弹窗：${dialog.innerText}`)
+			return false
 		}
-		suspend(1000)
+		await suspend(1000)
 		simulateClick(btn, true)
 	}
 	else {
 		console.log('不需要关注店铺，即将获取结果')
 	}
 
-	await waitForDialog()
-	console.log(`${activityId} 结果已经加载`)
+	if(await waitForDialog() === false){
+		return false
+	}
 
 	let result = document.querySelector('.ui-dialog') // 获取整个弹窗的值
 	return dealWithDialogAlert(result.innerText)
-}
-async function activityApply() {
-	console.log(`即将执行 activityApply`)
-	const activityId = parseActivityId(window.location.href)
-
-	let destoryIframeMsg = (status = false, success = false) => {
-		chrome.runtime.sendMessage({
-			action: "bg_activity_applied",
-			href: window.location.href,
-			activityId: activityId,
-			status: status,
-			success: success
-		})
-	}
-
-	let state = document.querySelector('.state')
-	if (!state) {                                 // 获取不了状态，关闭
-		console.log(`${window.location.href} 无法获取状态`)
-		return destoryIframeMsg(false) // window.location.reload()
-	}
-	if (state.innerText.startsWith('您已提交申请')) { // 已申请，关闭并更新状态
-		console.log(`${window.location.href} ${state.innerText}`)
-		return destoryIframeMsg(true)
-	}
-	if (!state.innerText.startsWith('活动已开始')) { // 当前页面不是可以申请的页面，关闭
-		console.log(`${window.location.href} ${state.innerText}`)
-		return destoryIframeMsg(false)
-	}
-
-	clickApplyBtn(activityId)
-		.then(res => {
-			destoryIframeMsg(res, res)
-		})
-		.catch(err => {
-			console.warn(err)
-			destoryIframeMsg()
-		})
-}
-
-function successActivityListRetrieval() {
-	console.log(`即将执行 successActivityListRetrieval`)
-	let activityList = document.querySelectorAll('.list-detail-item')
-	if (!activityList) {
-		console.warn('无法获得/没有申请成功的商品')
-		return
-	}
-	let successActivityList = []
-	for (let activity of activityList) {
-		const countDown = activity.querySelector('.re-time')
-		if (!countDown) {//过期了
-			continue
-		}
-		const timestamp = parseInt(countDown.getAttribute('end_time')) + countDown.getAttribute('after_hours') * 60 * 60 * 1000
-		successActivityList.push({
-			url: activity.querySelector('.p-name a').href,
-			id: parseInt(activity.getAttribute('activity_id')),
-			pic_url: activity.querySelector('img').src,
-			name: activity.querySelector('.p-name').innerText,
-			detail: activity.querySelector('.p-detail').innerText,
-			price: parseFloat(activity.querySelector('.p-price span').innerText.substr(1)),
-			timestamp: timestamp,
-			apply: true
-		})
-	}
-	chrome.runtime.sendMessage({
-		action: "success_activity_retrieval",
-		successActivityList: successActivityList,
-		href: window.location.href
-	})
-
-	const pages = document.querySelector('.page.clearfix .p-wrap .p-skip b')
-	chrome.runtime.sendMessage({
-		action: "bg_page_num_retrieval",
-		pageNum: pages ? pages.innerText : 0,
-		href: window.location.href,
-	})
 }
 
 async function emptyFollowVenderList() {
@@ -260,13 +340,10 @@ async function emptyFollowVenderList() {
 	}
 	simulateClick(unFollowBtn, true)
 
-	try {
-		await waitForDialog()
+	if(await waitForDialog() === false){
+		return false;
 	}
-	catch (e) {
-		console.warn('无法获得确定弹窗')
-		return false
-	}
+
 	const dialog = document.querySelector('.ui-dialog')
 	const submitBtn = dialog.querySelector('.ui-dialog-btn-submit')
 	if (!submitBtn || submitBtn.innerText !== '确定') {
@@ -275,135 +352,45 @@ async function emptyFollowVenderList() {
 	}
 	simulateClick(submitBtn, true)
 
-	try {  //等待 ajax post 完成，最多等10秒
-		await waitForDialog(10*1000)
-		console.warn(`${document.querySelector('.ui-dialog').innerText}`)
-	}
-	catch (e) {
-		console.warn(`无法获得 emptyFollowVenderList 操作结果/网络不好`)
-	}
+	await waitForDialog(10 * 1000)
 	return false
 }
 
-async function followNumberRetrieval() {
-	console.log(`即将执行 followNumberRetrieval`)
-	let num = 0
-	document.querySelectorAll('#categoryFilter li').forEach(li => {
-		const reg = /（(\d*)）/.exec(li.innerText)
-		if (reg) {
-			num += parseInt(reg[1])
-		}
-	})
-
-	console.log(`当前用户一共关注了 ${num} 个店铺`)
-
-	chrome.runtime.sendMessage({
-		action: "bg_follow_vender_num_retrieval",
-		followVenderNum: num
-	})
-
-}
-function autoLogin() {
-	console.log(`即将执行 autoLogin`)
-	simulateClick(document.querySelector('.login-tab.login-tab-r a'), true)
-	const autoLoginBtn = document.createElement('p')
-	autoLoginBtn.innerHTML = "<span class='jdtry-login'>让京试记住密码并自动登录</span>"
-	document.querySelector('.item.item-fore5').append(autoLoginBtn)
-	autoLoginBtn.onclick = () => {
-		const username = document.querySelector("#loginname").value
-		const password = document.querySelector("#nloginpwd").value
-		if (!username || !password) {
-			Toast('请填写账号和密码后重试')
-			return
-		}
-		storage.set({ account: { username: username, password: password } }).then(() => {
-			Toast('保存账号和密码成功')
-
-			chrome.runtime.sendMessage({
-				action: "bg_get_login_status",
-			}, loginStatus => {
-				console.log(loginStatus)
-				if (loginStatus.status !== USER_STATUS.LOGIN) {
-					simulateClick(document.querySelector(".login-btn a"), true)
-				}
-			})
-		})
-
-	}
-	storage.get({ autoLogin: false }).then(res => {
-
-		if (!res.autoLogin) {
-			return
-		}
-
-		suspend(2000).then(
-			storage.get({ account: { username: '', password: '' } })
-				.then(res => {
-					if (!res.account.username || !res.account.password) {
-						return
-					}
-					document.querySelector("#loginname").value = res.account.username
-					document.querySelector("#nloginpwd").value = res.account.password
-
-					simulateClick(document.querySelector(".login-btn a"), true)
-
-					setTimeout(() => {
-						console.warn('自动登录失败')
-					}, 3000)
-				})
-		)
-
-	})
-
-}
-
-window.onload = () => {
-	if(document.body.innerText === '{"bottom":null}'){
-		console.warn('{"bottom":null}')
-		location.reload(true)
-	}
-
-	const HREF = window.location.href
-	console.log(`${HREF} 已加载`)
-
-	checkLoginStatus()
-
-	if (document.querySelector('.login-tab.login-tab-r')) {
-		autoLogin()
+async function do_actions(actions){
+	if(actions === undefined){
 		return
 	}
+	for(let act of actions){
+		if(act in task_opts){
+			console.log(`do ${act} action`)
+			if(await task_opts[act]() === false){
+				console.log(`do ${act} action fail`)
+			}
+		}
+		else{
+			console.warn(`unknown ${act} action`)
+			break
+		}
+	}
+}
+window.onload = () => {
+	console.log(`${window.location.href} loading`)
 
 	const openByBrowser = self === top // 用于标示 是浏览器打开还是脚本使用iframe打开
 	if (openByBrowser) {
-		console.log('浏览器打开，不进行任何操作')
+		if (document.querySelector('.login-tab.login-tab-r')) {
+			insert_auto_login_btn()
+		}
 		return
 	}
 
 	setTimeout(() => {
-
-		if (HREF.startsWith('https://t.jd.com/vender/followVenderList.action')
-			|| HREF.startsWith('https://t.jd.com/follow/vender/list.do')) {
-			followNumberRetrieval()
-		}
-		if (HREF === 'https://t.jd.com/vender/followVenderList.action'
-			|| HREF === 'https://t.jd.com/follow/vender/list.do') {
-
-			emptyFollowVenderList().then(res => {
-				if (!res) {//要么成功自动reload, 要么失败手动reload
-					window.location.reload()
-				}
-			})
-
-		}
-		if (HREF.startsWith('https://try.jd.com/activity/getActivityList')) {
-			newActivityListRetrieval() // 不用登录也可以获取
-		}
-		if (HREF.search(/https:\/\/try.jd.com\/user\/myTrial\?page=\d*&selected=2/) >= 0) {
-			successActivityListRetrieval()
-		}
-		if (HREF.search(/https:\/\/try.jd.com\/\d*\.html/) >= 0) {
-			activityApply()
-		}
+		chrome.runtime.sendMessage({
+			action: "bg_task_opt",
+			data: { href: window.location.href }
+		},actions=>{
+			do_actions(actions)
+		})
 
 	}, 4000)
 
--- static/tasks.js
+++ static/tasks.js
@@ -1,77 +1,109 @@
 import { storage } from './utils'
+const NO_LIMIT = 0
 const ONE_HOUR = 60 * 60 * 1000
 const HALF_HOUR = 30 * 60 * 1000
 const TEN_MINUTE = 10 * 60 * 1000
+export const TASK_ID = {
+	FOLLOW_VENDER_NUM_RETRIEVAL:0,
+	EMPTY_FOLLOW_VENDER_LIST:1,
+	ACTIVITY_RETRIEVAL:2,
+	SUCCESS_ACTIVITY_RETRIEVAL:3,
+	ACTIVITY_APPLY:4,
+	CHECK_OR_DO_LOGIN_OPT:5,
+
+	RUNNING:6978
+}
+
+export const commonTasks = [
+	{
+		actions: ["auto_login"],
+		href_patterns: [/https:\/\/passport.jd.com/]
+	},
+	{
+		actions: ["check_login_status"],
+		href_patterns: [/https:\/\/try.jd.com/]
+	},
+	{
+		actions: ["check_login_status", "follow_vender_num_retrieval"],
+		href_patterns: ["https://t.jd.com/vender/followVenderList.action*", "https://t.jd.com/follow/vender/list.do*"]
+	},
+	{
+		actions: ["check_login_status", "success_activity_retrieval"],
+		href_patterns: [/https:\/\/try.jd.com\/user\/myTrial\?page=\d*&selected=2/]
+	}
+]
 export const defaultTasks = [
 	{
-		id: 0,
+		id: TASK_ID.FOLLOW_VENDER_NUM_RETRIEVAL,
 		title: "获取关注数量",
 		description: "获取关注店铺的数量，请及时清理",
-		action: 'follow_vender_num_retrieval',
 		last_run_at: 0,
 		auto: {
 			run: false,
 			when: 11,
-			frequency: 'nolimit',
-			taskLifetime: TEN_MINUTE,
+			frequency: NO_LIMIT,
 		},
-		checkLogin:true,
-	}, {
-		id: 1,
+		actions: ["check_login_status", "follow_vender_num_retrieval"],
+		href_patterns: ["https://t.jd.com/vender/followVenderList.action*", "https://t.jd.com/follow/vender/list.do*"]
+	},
+	{
+		id: TASK_ID.EMPTY_FOLLOW_VENDER_LIST,
 		title: "清空关注列表",
 		description: "取关所有已关注的店铺",
-		action: 'empty_follow_vender_list',
 		last_run_at: 0,
 		auto: {
 			run: false,
 			when: 11,
-			frequency: 'daily',
-			taskLifetime: HALF_HOUR,
+			frequency: NO_LIMIT,
 		},
-		checkLogin:true,
-	}, {
-		id: 2,
+		actions: ["check_login_status", "follow_vender_num_retrieval", "empty_follow_vender_list"],
+		href_patterns: ["https://t.jd.com/vender/followVenderList.action*", "https://t.jd.com/follow/vender/list.do*"]
+	},
+	{
+		id: TASK_ID.ACTIVITY_RETRIEVAL,
 		title: "搜索商品",
 		description: "搜索并入库指定条件下的京东试用商品列表",
-		action: 'activity_retrieval',
 		last_run_at: 0,
 		auto: {
 			run: false,
 			when: 11,
-			frequency: 'daily',
-			taskLifetime: HALF_HOUR,
+			frequency: NO_LIMIT
 		},
-		checkLogin:false,
-	}, {
-		id: 3,
+		actions: ["activity_retrieval"],
+		href_patterns: ["https://try.jd.com/activity/getActivityList*"]
+	},
+	{
+		id: TASK_ID.SUCCESS_ACTIVITY_RETRIEVAL,
 		title: "检索成功",
 		description: "检查是否有新申请成功的项目",
-		action: 'success_activity_retrieval',
 		last_run_at: 0,
 		auto: {
 			run: false,
 			when: 11,
-			frequency: 'nolimit',
-			taskLifetime: TEN_MINUTE
+			frequency: NO_LIMIT,
 		},
-		checkLogin:true,
-	}, {
-		id: 4,
+		actions: ["check_login_status", "success_activity_retrieval"],
+		href_patterns: [/https:\/\/try.jd.com\/user\/myTrial\?page=\d*&selected=2/]
+	},
+	{
+		id: TASK_ID.ACTIVITY_APPLY,
 		title: "一键申请",
 		description: "立即执行申请试用商品任务",
-		action: 'activity_apply',
 		last_run_at: 0,
 		auto: {
 			run: false,
 			when: 11,
-			frequency: 'daily',
-			taskLifetime: ONE_HOUR
+			frequency: NO_LIMIT
 		},
-		checkLogin:true,
-	}
+		actions: ["check_login_status", "activity_apply"],
+		href_patterns: [/https:\/\/try.jd.com\/\d*\.html/]
+	},
 ]
 
 export function updateTaskInfo(task) {
+	if(typeof (activityId) === 'number'){
+		return
+	}
 	const taskKey = `task_${task.id}`
 	storage.set({ [taskKey]: task })
 }
